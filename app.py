95cefbf75b30e296dc5870ffd771bd7cfb2f2f29
Aftenius <timofej.makarenko@mail.ru>
2023-10-11 00:58:20 +0300

    Many

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+# tag_post = db.Table('tag_post',
+#                     db.Column('post_id', db.Integer, db.ForeignKey('shop.id')),
+#                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+#                     )
+
+
 class Shop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     intro_text = db.Column(db.String(300), nullable=False)
     main_text = db.Column(db.Text, nullable=False)
-    category = db.Column(db.Integer)
+
+    # category = db.relationship('Tag', secondary=tag_post, backref='tag')
 
     def __repr__(self):
         return '<Frticle %r>' % self.id
@@ -27,6 +34,40 @@ class Comment(db.Model):
     def __repr__(self):
         return '<Frticle %r>' % self.id
 
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String(500))
+
+    def __repr__(self):
+        return '<Frticle %r>' % self.id
+
+
+# тестирование MANY TO MANY
+user_channel = db.Table('user_channell',
+                         db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                         db.Column('channel_id', db.Integer, db.ForeignKey('channel.id'))
+                         )
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    following = db.relationship('Channel', secondary=user_channel, backref='followers')
+
+    def __repr__(self):
+        return f'<user: {self.name}>'
+
+
+class Channel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<Channel: {self.name}>'
+
+# ------------------------------- END ---------------------------------------
+
 @app.route('/sign')
 def sign_in():
     return render_template("sign.html")
@@ -39,9 +80,17 @@ def create_post():
         intro_text = request.form['intro_text']
         main_text = request.form['main_text']
 
+        tag_name = request.form['tag_name'].split()
+        print(tag_name)
+
         product = Shop(title=title, intro_text=intro_text, main_text=main_text)
 
         try:
+            for tag in tag_name:
+                tags = Tag(tag_name=tag)
+                db.session.add(tags)
+                db.session.commit()
+
             db.session.add(product)
             db.session.commit()
             return redirect('/')
@@ -61,7 +110,10 @@ def index():
 @app.route('/product/<int:id>/', methods=['POST', 'GET'])
 def product(id):
     id_product = Shop.query.get(id)
-    com_product = Comment.query.order_by(Comment.id.desc()).all()
+    com_product = Comment.query.filter_by(id_product_com=id).order_by(Comment.id.desc()).all()
+    # post_tag = post_Tag.query.filter_by(post_id=id).with_entities(post_Tag.tag_id).all()
+    tag = Tag.query.get(id)
+    # print(post_tag)
 
     if request.method == "POST":
         id_product_com = id
@@ -75,10 +127,11 @@ def product(id):
             db.session.commit()
             return redirect(f'/product/{id}')
         except:
-            return "произошла ошибка"
+            return "пошибка"
 
     else:
-        return render_template("product.html", id_product=id_product, com_product=com_product)
+        return render_template("product.html", id_product=id_product, com_product=com_product, tag=tag)
+
 
 @app.route('/product/<int:id>/del')
 def product_del(id):
